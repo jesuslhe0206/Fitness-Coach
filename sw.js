1613f1f308ebe691eae92ad2ca217bfed479bc3e
jesuslhe0206 <jesuslhe0206@gmail.com>
2026-08-19 15:17:58 -0600

    Bump PWA cache after core image fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitness-coach-v4';
+const CACHE='fitness-coach-v5';
 const CORE=['/','/index.html','/manifest.webmanifest','/exercise-images-patch.js'];
 
 self.addEventListener('install',e=>{
